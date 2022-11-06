a2d09c763b678f485624df503fd67b598046d465
HaithemGhattass <haithem.ghattas@esprit.tn>
2022-11-06 02:23:55 +0100

    first empty project

---
+++ clothy/clothy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clothy
+//
+//  Created by haithem ghattas on 6/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clothy/clothy/clothyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothyApp.swift
+//  clothy
+//
+//  Created by haithem ghattas on 6/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct clothyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
